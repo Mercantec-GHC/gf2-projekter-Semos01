c19733c6647f174873282c80c924549ea99e7b3e
Semos01 <mikkelchrlarsen@outlook.dk>
2025-09-10 15:03:21 +0200

    Update GuessANumber.cs

--- Projekter/Konsol/Hjemmet/GuessANumber.cs
+++ Projekter/Konsol/Hjemmet/GuessANumber.cs
@@ -5,6 +5,13 @@ namespace Hjemmet
         public void Start()
         {
             Console.WriteLine("Gæt et tal er ikke implementeret endnu.");
+
+            Random tilfældigtal = new Random();
+            int nummeratgætte = tilfældigtal.Next(1, 101);
+            Console.WriteLine();
+            Console.WriteLine(nummeratgætte);
+            Console.WriteLine();
+
             Console.ReadKey();
         }
     }
